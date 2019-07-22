494a76f4c2f5360a5ad82c2a1d43be345ad54cd6
Adam <skiryoland@gmail.com>
2019-07-22 17:36:37 +0200

    Added Lambda Branch

--- src/main/java/org/adam/aliasswitcher/AliasSwitcherApplication.java
+++ src/main/java/org/adam/aliasswitcher/AliasSwitcherApplication.java
@@ -1,10 +1,16 @@
 package org.adam.aliasswitcher;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AliasSwitcherApplication {
+public class AliasSwitcherApplication extends SpringBootServletInitializer {
+
+    // silence console logging
+    @Value("${logging.level.root:OFF}")
+    String message = "";
 
     public static void main(String[] args) {
         SpringApplication.run(AliasSwitcherApplication.class, args);
