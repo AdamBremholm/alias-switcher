a384d2d8fbc0855c7f855f23c22bc3a04a4e3ef0
AdamBremholm <skiryoland@gmail.com>
2019-10-09 18:08:20 +0200

    Move user details to application.properties

--- src/main/java/org/adam/aliasswitcher/jwt/JwtUserDetailsService.java
+++ src/main/java/org/adam/aliasswitcher/jwt/JwtUserDetailsService.java
@@ -1,6 +1,8 @@
 package org.adam.aliasswitcher.jwt;
 
 import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,14 +10,20 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
+
+    @Value("${user.adam}")
+    String bcryptHashAdam;
+    @Value("${user.erika}")
+    String bcryptHashErika;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         if ("adam".equals(username)) {
-            return new User("adam", "$2a$10$guspLDrMPOoI1N6rNIszHe30g4jhhioA4OJvXao3N8voyUQVeJq46",
+            return new User("adam", bcryptHashAdam,
                     new ArrayList<>());
         }
         else if ("erika".equals(username)) {
-            return new User("erika", "$2a$10$4CvlgUtTImgoLhM419jsjOjLDYmqmJHk3FUOmxuGmujoouCRNjbce",
+            return new User("erika", bcryptHashErika,
                     new ArrayList<>());
 
         }
